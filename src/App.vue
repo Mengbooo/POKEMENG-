1eedc66e6ee2166ddbcc43c30426fc6039568b02
QMengbooo <Bo156431362@outlook.com>
2024-05-20 19:49:22 +0800

    What can I say!Manba OUT!

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+</script>
+
+<template>
+  <div style="position: absolute;top: 30px;transform: translateX(-50%);user-select: none;">
+    <a href="https://github.com/Mengbooo"><img src="./assets/logoooo.jpg" alt=""
+        style="width: 50px; border-radius: 10px;"></a>
+  </div>
+
+  <div class="container">
+    <div class="card" style="--i: -4;">Wh</div>
+    <div class="card" style="--i: -3;">at</div>
+    <div class="card" style="--i: -2;">can</div>
+    <div class="card" style="--i: -1;">I</div>
+    <div class="card" style="--i: -0;">say</div>
+    <div class="card" style="--i: 1;">man</div>
+    <div class="card" style="--i: 2;">ba</div>
+    <div class="card" style="--i: 3;">out</div>
+    <div class="card" style="--i: 4;">!</div>
+  </div>
+  <div style="user-select: none;position: absolute;bottom: 20px;transform: translateX(-50%);color: white;z-index: 999;">
+    <p>Thanks for the inspiration from <a href="https://www.youtube.com/@OnlineTutorialsYT"
+        style="color: aqua;text-decoration: none;"><strong>OnlineTutorialsYT</strong></a></p>
+    <p>Remastered by <a style="color: aqua;text-decoration: none;"
+        href="https://github.com/Mengbooo"><strong>Bolaxious</strong></a></p>
+  </div>
+
+</template>
+
+<style scoped></style>
